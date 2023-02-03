b2dee860a476e314ead15c652a6ec43c7cbe7577
Hemanth Kumar <70285394+Hemanth-Kumar-G@users.noreply.github.com>
2023-02-04 04:42:42 +0530

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by HEMANTH on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
